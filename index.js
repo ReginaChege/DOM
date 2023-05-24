53702f29771258e6dfe60142c25e4dd3b702fdcc
ReginaChege <reginachegewairimu@gmail.com>
2023-05-24 16:33:31 +0300

    second commit

--- index.js
+++ index.js
@@ -1,11 +1,6 @@
 document.getElementById('body').style.backgroundColor= 'silver'
 document.getElementById('title').style.color='green'
-document.getElementById()
-
-for(let i=0;i<h3Elemen.length;i++)
-h3elemnt[i].style.textTransform='uppercase'
-let li=documents.createElement('li')
-
+let li=createElement('li')
 li.innerHTMl='Oranges'
 document.getElementById('fruList').appendChild(li)
 
