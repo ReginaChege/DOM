66c53cb236ef19ac2fb7d4f18ce81bf1629f33ab
ReginaChege <reginachegewairimu@gmail.com>
2023-05-24 16:27:37 +0300

    first

---
+++ index.js
@@ -0,0 +1,17 @@
+document.getElementById('body').style.backgroundColor= 'silver'
+document.getElementById('title').style.color='green'
+document.getElementById()
+
+for(let i=0;i<h3Elemen.length;i++)
+h3elemnt[i].style.textTransform='uppercase'
+let li=documents.createElement('li')
+
+li.innerHTMl='Oranges'
+document.getElementById('fruList').appendChild(li)
+
+let li=documents.createElement('li')
+li.innerHTMl='Cabbage'
+document.getElementById('VegList').appendChild(li)
+
+
+
