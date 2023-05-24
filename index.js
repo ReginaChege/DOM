18797c5e602cece17e1a3ad427dcc94b5888b830
ReginaChege <reginachegewairimu@gmail.com>
2023-05-24 17:02:15 +0300

    changes

--- index.js
+++ index.js
@@ -1,12 +1,16 @@
 document.getElementById('body').style.backgroundColor= 'silver'
 document.getElementById('title').style.color='green'
-let li=createElement('li')
-li.innerHTMl='Oranges'
+
+let li=document.createElement('li')
+li.innerHTMl='pears'
+document.createElement('li')
 document.getElementById('fruList').appendChild(li)
 
-let li=documents.createElement('li')
-li.innerHTMl='Cabbage'
-document.getElementById('VegList').appendChild(li)
+let lii=documents.createElement('li')
+lii.innerHTMl='Cabbage'
+document.createElement(lii)
+document.getElementById('VegList').appendChild(lii)
+
 
 
 
